3bc559e75a8eb781c3f2036a33f33ea03ac1193c
spreered <spreered@5xruby.tw>
2020-03-10 00:45:29 +0800

    add bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+echo "installing dotfile..."
+
+ln -s ~/.dotfile/vim ~/.vim 
+ln -s ~/.vim/vimrc ~/.vimrc
+ln -s ~/.dotfile/zshrc ~/.zshrc
+ln -s ~/.dotfile/zsh ~/.zsh
+ln -s ~/.dotfile/zshenv ~/.zshenv
+ln -s ~/.dotfile/Brewfile ~/Brewfile
