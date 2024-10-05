50fadb18a8545cb68c9e9b4c40bbcaa42b257d4f
spreered <spreered@gmail.com>
2024-10-05 12:46:15 +0800

    Bootstrap nvim config

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,8 +3,9 @@
 
 echo "installing dotfile..."
 
-ln -s `pwd`/vim $HOME/.vim 
-ln -s `pwd`/vimrc $HOME/.vimrc
+# ln -s `pwd`/vim $HOME/.vim 
+# ln -s `pwd`/vimrc $HOME/.vimrc
+ln -s `pwd`/nvim ~/.config/nvim
 ln -s `pwd`/zshrc ~/.zshrc
 ln -s `pwd`/zsh ~/.zsh
 ln -s `pwd`/zshenv ~/.zshenv
